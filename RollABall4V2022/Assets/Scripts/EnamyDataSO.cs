ceb79f6111adee29e60db1a75b0ff0cbd8119e72
Rafael <h.batista@escolar.ifrn.edu.br>
2022-11-21 17:57:47 -0300

    enemy

---
+++ RollABall4V2022/Assets/Scripts/EnamyDataSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityTemplateProjects
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class EnamyDataSO : ScriptableObject
+    {
+        
+    }
+}
